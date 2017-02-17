b77f05e926338b10ef1f983e69b9090dcdc8c105
Mashrin Srivastava <mashrin.msrivastava2014@vit.ac.in>
2017-02-18 04:42:22 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -28,63 +28,60 @@ if (!$conn) {
 <html lang="en">
 
 <head>
-    <!-- Basic Page Needs
-    ================================================== -->
+    
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>AUREOLE Techno Cultural fest 2016 : Contact</title>
-    <!-- Mobile Specific Metas
-    ================================================== -->
+    
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSS
-    ================================================== -->
-    <!-- Bootstrap css file-->
+    
+    
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font awesome css file-->
+    
     <link href="css/font-awesome.min.css" rel="stylesheet">
-    <!-- Superslide css file-->
+    
     <link rel="stylesheet" href="css/superslides.css">
-    <!-- Slick slider css file -->
+    
     <link href="css/slick.css" rel="stylesheet">
-    <!-- Circle counter cdn css file -->
+    
     <link rel='stylesheet prefetch' href='https://cdn.rawgit.com/pguso/jquery-plugin-circliful/master/css/jquery.circliful.css'>
-    <!-- smooth animate css file -->
+    
     <link rel="stylesheet" href="css/animate.css">
-    <!-- preloader -->
+    
     <link rel="stylesheet" href="css/queryLoader.css" type="text/css" />
-    <!-- gallery slider css -->
+    
     <link type="text/css" media="all" rel="stylesheet" href="css/jquery.tosrus.all.css" />
-    <!-- Default Theme css file -->
+    
     <link id="switcher" href="css/themes/default-theme.css" rel="stylesheet">
-    <!-- Main structure css file -->
+    
     <link href="style.css" rel="stylesheet">
-    <!-- Google fonts -->
+    
     <link href='http://fonts.googleapis.com/css?family=Merriweather' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Varela' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
-    <!-- SCROLL TOP BUTTON -->
+    
     <a class="scrollToTop" href="#"></a>
-    <!-- END SCROLL TOP BUTTON -->
-    <!--=========== BEGIN HEADER SECTION ================-->
+    
+    
     <header id="header">
-        <!-- BEGIN MENU -->
+        
         <div class="menu_area">
             <nav class="navbar navbar-default" role="navigation">
                 <div class="container">
                     <div class="navbar-header">
-                        <!-- FOR MOBILE VIEW COLLAPSED BUTTON -->
+                        
                         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                             <span class="sr-only">Toggle navigation</span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <!-- LOGO -->
-                        <!-- IMG BASED LOGO  -->
+                        
+                        
                         <a class="navbar-brand" href="index.html"><img src="logo.jpg" alt="logo" height="100" width="250"></a>             
-                        <!-- TEXT BASED LOGO -->
+                        
                         <a class="navbar-brand" href="index.html"><span style="color:orange">&nbsp;&nbsp;&nbsp;AUREOLE</span> &nbsp;<span style="color:white">TECHNO </span> <span> CULTURAL <span style="color:white"> FEST</span> <span style="color:green">2016</span></a>
                     </div>
                     <div id="navbar" class="navbar-collapse collapse">
@@ -94,26 +91,26 @@ if (!$conn) {
                             <li><a href="events-archive.html">Mega Events</a></li>
                             <li><a href="hospitality.html">Hospitality</a></li>
                             <li><a href="gallery.html">Gallery</a></li>
-                            <!-- <li><a href="sponser-archive.html">SPONSORS</a></li> -->
+                            
                             <li><a href="reg.php">Registration</a></li>
                             <li class="active"><a href="contact.php">Contact</a></li>
                         </ul>
                     </div>
-                    <!--/.nav-collapse -->
+                    
                 </div>
             </nav>
         </div>
-        <!-- END MENU -->
+        
     </header>
-    <!--=========== END HEADER SECTION ================-->
-    <!--=========== BEGIN COURSE BANNER SECTION ================-->
+    
+    
     <br></br>
     <br></br>
     <section id="imgBanner">
-        <!-- <h2>Contact</h2> -->
+        
     </section>
-    <!--=========== END COURSE BANNER SECTION ================-->
-    <!--=========== BEGIN CONTACT SECTION ================-->
+    
+    
     <section id="contact">
         <div class="container">
             <div class="row">
@@ -165,14 +162,14 @@ if (!$conn) {
             </div>
         </div>
     </section>
-    <!--=========== END CONTACT SECTION ================-->
-    <!--=========== BEGIN GOOGLE MAP SECTION ================-->
+    
+    
     <section id="googleMap">
         <<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3667.4059978630294!2d79.9848337!3d23.1918702!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3981aedd33095901%3A0xdc3ab3465781aba6!2sJabalpur+Engineering+College!5e0!3m2!1sen!2sin!4v1453575563513" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
     </section>
-    <!--=========== END GOOGLE MAP SECTION ================-->
+    
     <br>
-    <!--=========== BEGIN OUR TEAM SECTION ================-->
+    
     <section id="ourTutors">
       <div class="container">
           <div class="row">
@@ -183,13 +180,13 @@ if (!$conn) {
             </div>
           </div>
         </div>
-        <!-- End Our courses titile -->
+        
 
-        <!-- Start Our courses content -->
+        
         <div class="row">
           <div class="col-lg-12 col-md-12 col-sm-12">
             <div class="ourTutors_content">
-              <!-- Start Tutors nav -->
+              
               <ul class="tutors_nav">
                 <li>
                   <div class="single_tutors">
@@ -533,11 +530,11 @@ if (!$conn) {
         </div>
       </div>
     </section>
-    <!--=========== END OUR Team SECTION ================-->
+    
 
-    <!--=========== BEGIN FOOTER SECTION ================-->
+    
     <footer id="footer">
-        <!-- Start footer top area -->
+        
         <div class="footer_top">
             <div class="container">
                 <div class="row">
@@ -574,8 +571,8 @@ if (!$conn) {
                 </div>
             </div>
         </div>
-        <!-- End footer top area -->
-        <!-- Start footer bottom area -->
+        
+        
         <div class="footer_bottom">
             <div class="container">
                 <div class="row">
@@ -592,30 +589,29 @@ if (!$conn) {
                 </div>
             </div>
         </div>
-        <!-- End footer bottom area -->
+        
     </footer>
-    <!--=========== END FOOTER SECTION ================-->
-    <!-- Javascript Files
-    ================================================== -->
-    <!-- initialize jQuery Library -->
+    
+    
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Preloader js file -->
+    
     <script src="js/queryloader2.min.js" type="text/javascript"></script>
-    <!-- For smooth animatin  -->
+    
     <script src="js/wow.min.js"></script>
-    <!-- Bootstrap js -->
+    
     <script src="js/bootstrap.min.js"></script>
-    <!-- slick slider -->
+    
     <script src="js/slick.min.js"></script>
-    <!-- superslides slider -->
+    
     <script src="js/jquery.easing.1.3.js"></script>
     <script src="js/jquery.animate-enhanced.min.js"></script>
     <script src="js/jquery.superslides.min.js" type="text/javascript" charset="utf-8"></script>
-    <!-- for circle counter -->
+    
     <script src='https://cdn.rawgit.com/pguso/jquery-plugin-circliful/master/js/jquery.circliful.min.js'></script>
-    <!-- Gallery slider -->
+    
     <script type="text/javascript" language="javascript" src="js/jquery.tosrus.min.all.js"></script>
-    <!-- Custom js-->
+    
     <script src="js/custom.js"></script>
 </body>
 
